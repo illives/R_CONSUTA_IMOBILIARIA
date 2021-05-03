339be749294b3a5f4ae7b8a5ffb534721855731d
illives <illives9455@hotmail.com>
2021-05-03 13:30:22 -0300

    Ajuste Variavel de Ambiente

--- Scrypt/main.py
+++ Scrypt/main.py
@@ -4,6 +4,8 @@ from Resources.web_resources import web_tools
 #-----------------Variaveis de Ambiente---------------------------------------------------
 driverpath = 'D:\\02.NOVOBOT\\R_CONSULTA_IMOB\\Driver\\chromedriver.exe'
 pathfile = 'D:\\02.NOVOBOT\\R_CONSULTA_IMOB\\File\\'
+driverpath = 'D:\\02.GIT_REPOSITORY\\R_CONSULTA_IMOB\\R_CONSUTA_IMOBILIARIA\\Driver\\chromedriver.exe'
+pathfile = 'D:\\02.GIT_REPOSITORY\\R_CONSULTA_IMOB\\R_CONSUTA_IMOBILIARIA\\File'
 #----------------------------------------------------------------------------------------
 print('R_CONSULTA_IMOB\nDesenvolvido por William Souza (c) Copyright\n')
 
